136356c8a6be035b74fc6a0487872c7d5f7f2e77
Lesia Humenna <lgumenna61@gmail.com>
2023-08-11 13:28:36 +0200

    add get set/ delete console.log(this/global)

--- main.js
+++ main.js
@@ -12,45 +12,50 @@ class Hamburger {
     this.topping = topping;
     this.additivesProd = [];
   }
-  addProd() {
+  getaddProd() {
+    return this.additivesProd;
+  }
+ setaddProd() {
     this.additivesProd.push(this.topping);
   };
+ 
   calculateTotalPrice() {
     let totalPrice = this.size.price + this.stuffing.price;
 
     for (let toppings of this.additivesProd) {
-        totalPrice += toppings.price;
+        totalPrice += toppings?.price || 0;
     }
     return totalPrice;
   };
   calculateTotalCalories() {
     let totalCalories = this.size.calories + this.stuffing.calories;
-    for (let toppings of this.additivesProd) {
-        totalCalories += toppings.calories;
+    for(let toppings of this.additivesProd) {
+        totalCalories += toppings?.calories || 0;
     }
     return totalCalories;
+
 }
 };
 // hamburger1
-const hamburger1 = new Hamburger (SIZE_SMALL, STUFFING_CHEESE, STUFFING_SALATH, STUFFING_POTATIONS, [this.additivesProd]);
+const hamburger1 = new Hamburger (SIZE_SMALL, STUFFING_CHEESE, []);
 console.log(hamburger1) ;
-hamburger1.addProd(TOPPING_MAYO);
+hamburger1.getaddProd(TOPPING_MAYO);
 console.log("Calories: " + hamburger1.calculateTotalCalories());
 console.log("Price: " + hamburger1.calculateTotalPrice());
-hamburger1.addProd(TOPPING_SPICE);
+hamburger1.getaddProd(TOPPING_SPICE);
 console.log("Price with sauce: " + hamburger1.calculateTotalPrice());
 //hamburger2
-const hamburger2 = new Hamburger (SIZE_BIG, STUFFING_CHEESE, STUFFING_SALATH, [this.additivesProd]);
+const hamburger2 = new Hamburger (SIZE_BIG, STUFFING_SALATH, []);
 console.log(hamburger2) ;
-hamburger2.addProd(TOPPING_MAYO);
+hamburger2.getaddProd(TOPPING_MAYO);
 console.log("Calories: " + hamburger2.calculateTotalCalories());
 console.log("Price: " + hamburger2.calculateTotalPrice());
-hamburger2.addProd(TOPPING_SPICE);
+hamburger2.getaddProd(TOPPING_SPICE);
 console.log("Price with sauce: " + hamburger2.calculateTotalPrice());
 //hamburger3
-const hamburger3 = new Hamburger (SIZE_BIG, STUFFING_CHEESE, STUFFING_SALATH, STUFFING_POTATIONS, [this.additivesProd]);
+const hamburger3 = new Hamburger (SIZE_BIG, STUFFING_POTATIONS, []);
 console.log(hamburger3) ;
-hamburger3.addProd(TOPPING_SPICE);
+hamburger3.getaddProd(TOPPING_SPICE);
 console.log("Calories: " + hamburger3.calculateTotalCalories());
 console.log("Price: " + hamburger3.calculateTotalPrice());
 console.log("Price with sauce: " + hamburger3.calculateTotalPrice());
